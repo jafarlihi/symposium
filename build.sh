1843bc2bec588522235bedbfdbeb15bbe62ed8ad
Hikmat Jafarli <jafarlihi@gmail.com>
2020-05-25 12:08:03 +0400

    Fix build.sh

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 export API_URL=127.0.0.1:8081
 
 mkdir build
-cd ./api/ && go build && cp ./api ../build && cp -R ./public ../build && cd ..
+cd ./api/ && go build && cp ./api ../build && cp -R ./public ../build && cp ./config.json ../build && cp ./schema.sql ../build && cd ..
 cd ./ui/ && npm run build && cp -R ./dist/* ../build/public
